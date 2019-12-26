6514dd3ed815f491508825fb6380106cc6aecaad
abdulbosit <abdulbosit.khamidov@inoviagroup.se>
2019-12-26 23:18:22 +0500

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Helmet, HelmetProvider } from 'react-helmet-async'
+import 'utils/fonts'
+import styles from './App.scss'
+
+const App = () => {
+  return (
+    <HelmetProvider>
+      <div className={styles.App}>
+        <Helmet>
+          <title>React-G</title>
+        </Helmet>
+        <span data-test="greeting" className={styles.Greeting}>
+          Welcome to React-G!
+        </span>
+      </div>
+    </HelmetProvider>
+  )
+}
+
+export default App
